9527fb77d7a601778bf1200ae659f99557d8f458
Harsh <115858346+harshprajapati460@users.noreply.github.com>
2023-09-17 23:53:09 +0530

    Added count subset with given sum

--- count_subset_with_given_sum.cpp
+++ count_subset_with_given_sum.cpp
@@ -19,13 +19,37 @@ int count_subset(vector<int>arr,int n,int sum){
 }
 // MEMOIZATION AND RECURSION REMAIN SAME LOGIC
 
+int count_subset_recursion(vector<int>arr,int n,int sum){
+    if(sum == 0) return 1;
+    if(n == 0) return 0;
+
+    if(arr[n-1] > sum){
+        return count_subset_recursion(arr,n-1,sum);
+    }
+    else return  count_subset_recursion(arr,n-1,sum-arr[n-1]) + count_subset_recursion(arr,n-1,sum);
+}
+
+int count_subset_memoization(vector<int>arr,int n,int sum,vector<vector<int>>&dp){
+    if(sum == 0) return 1;
+    if(n == 0) return 0;
+
+    if(dp[n][sum] != -1){
+        return dp[n][sum];
+    }
+    if(arr[n-1] > sum){
+        return dp[n][sum] =  count_subset_recursion(arr,n-1,sum);
+    }
+    else return dp[n][sum] =  count_subset_recursion(arr,n-1,sum-arr[n-1]) + count_subset_recursion(arr,n-1,sum);
+}
+
 int main(){
 
 vector<int>arr = {2,3,5,8,10};
 int n = arr.size();
 int sum = 10;
-
+vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+cout<<count_subset_recursion(arr,n,sum)<<endl;
+cout<<count_subset_memoization(arr,n,sum,dp)<<endl;
 cout<<count_subset(arr,n,sum)<<endl;
-
 return 0;
 }
